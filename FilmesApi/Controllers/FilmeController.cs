60109fe548d66a273aaf7008b4b72fa14b7c3ebe
Augusto <victor.augusto212@gmail.com>
2024-06-02 17:24:03 -0300

    commit reste content

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -2,20 +2,42 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FilmesApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class FilmeController
+    public class FilmeController : ControllerBase
     {
         private static List<Filme> FilmesApi = new List<Filme>();
+        private static int Id = 1;
 
         [HttpPost]
-        public void AdicionarFilmes([FromBody] Filme filme)
+        public IActionResult AdicionarFilmes([FromBody] Filme filme)
         {
+            filme.Id = Id++;
             FilmesApi.Add(filme);
             Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmePorId),new {Id = filme.Id}, filme);
+        }
+
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(FilmesApi);
+        }
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            Filme filme =  FilmesApi.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+                
+            }
+            return Ok(filme);
         }
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,10 +4,13 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo titulo é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor {  get; set; }
+        [StringLength(30, ErrorMessage = "O generi nao pode passar de 30 carcters")]
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 e no maximo 600 minutos")]
         public int Duracao { get; set; }
